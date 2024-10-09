893b7f259c0fc10ae414da553dddee35487dfa6a
Enes Babekoğlu <92182480+enesbabekoglu@users.noreply.github.com>
2024-10-09 22:10:57 +0300

    Create example-1.cs

---
+++ example-1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        // Nesnenin başlangıç konumunu (0, 5, 0) olarak ayarlıyoruz
+        transform.position = new Vector3(0, 5, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
